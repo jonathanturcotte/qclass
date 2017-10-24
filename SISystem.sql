996f927d60f202d8bded6ac8b3959d4b46996708
demerah <curtis.demerah@queensu.ca>
2017-10-24 16:31:13 -0400

    Updated database file

--- SISystem.sql
+++ SISystem.sql
@@ -7,8 +7,8 @@ create table course (
 	cID			char(36) not null,
 	cCode 		varchar(10) not null,
 	cName		varchar(20),
-	regNum		char(5) not null,
 	defLocation	varchar(30),
+	pNetID		varchar(10) not null,
 	primary key (cID)
 );
 
@@ -32,12 +32,28 @@ create table enrolled (
 	cID			char(36) not null,
 	primary key (sNetID, cID)
 );
-	
+
+create table attendanceSession (
+	cID			char(36) not null,
+	attTime 	int not null,
+	attDuration	int not null,
+	primary key (cID, attTime)
+);
+
+create table attendance (
+	attTime 	int not null,
+	cID		char(36) not null,
+	sNetID 	varchar(10) not null,
+	primary key (cID, attTime, sNetID)
+);
+
 create table teaches (
 	pNetID		varchar(10) not null,
 	cID			char(36) not null,
 	primary key (pNetID, cID)
 );
+
+/*
 
 create table lecture (
 	lecNum		int not null,
@@ -47,13 +63,6 @@ create table lecture (
 	location	varchar(30),
 	lFlag		tinyint(1),
 	primary key (lecNum, cID)
-);
-
-create table attendance (
-	cID		char(36) not null,
-	lecNum 	int not null,
-	sNetID 	varchar(10) not null,
-	primary key (cID, lecNum, sNetID)
 );
 
 create table question (
@@ -84,4 +93,59 @@ create table response (
 	correct		tinyint(1),
 	primary key (qNum, lecNum, cID, sNetID)
 );
+
+*/
 	
+insert into course values 
+	( 'abc49eb2-0630-4382-98b5-abcfd40627b8', 'ELEC 498', 'Design Course', 'Dupuis Auditorium', '1pvb69'),
+	( 'ddd49eb2-0630-4382-98b5-lolfd40627b8', 'ECON 222', 'Macroeconomic Theory I', 'Boterall Hall', '10yfl1'),
+	( 'you49eb2-0630-4382-98b5-wutfd40627b8', 'ECON 360', 'Labour Economics', 'John Watson Hall', '15jc3'),
+	( 'ree49eb2-0630-4382-98b5-reefd40627b8', 'ECON 255', 'Math Econ', 'MacDonald Hall', '10boo3'),
+	( 'boo49eb2-0630-4382-98b5-moofd40627b8', 'CMPE 333', 'Data Mining', 'Kingston Hall', '12hdm');	
+
+
+insert into student values
+	( '12cjd2', 'Curtis', 'Demerah', '10090510'),
+	( '12ozs', 'Omar', 'Sandarusi', '10030050'),
+	( '11jlt10', 'Jonathan', 'Turcotte', '10060060');
+
+
+insert into  professor values
+	('1pvb69', 'Patrick', 'Van Blunt'),
+	('10yfl1', 'Yan-Fei', 'Liu'),
+	('15jc3', 'John', 'Cena'),
+	('10boo3', 'Bob', 'Orwell'),
+	('12hdm', 'Harold', 'McMaster');
+	
+insert into enrolled values 	
+	('12cjd2', 'abc49eb2-0630-4382-98b5-abcfd40627b8'),
+	('12ozs', 'abc49eb2-0630-4382-98b5-abcfd40627b8'),
+	('11jlt10', 'abc49eb2-0630-4382-98b5-abcfd40627b8'),
+	('12cjd2', 'you49eb2-0630-4382-98b5-wutfd40627b8'),
+	('12ozs', 'ree49eb2-0630-4382-98b5-reefd40627b8'),
+	('11jlt10', 'boo49eb2-0630-4382-98b5-moofd40627b8');
+
+insert into teaches values 
+	('abc49eb2-0630-4382-98b5-abcfd40627b8', '1pvb69'),
+	('ddd49eb2-0630-4382-98b5-lolfd40627b8', '10yfl1'),
+	('you49eb2-0630-4382-98b5-wutfd40627b8', '15jc3'),
+	('ree49eb2-0630-4382-98b5-reefd40627b8', '10boo3'),
+	('boo49eb2-0630-4382-98b5-moofd40627b8',  '12hdm');
+
+
+
+
+
+
+
+
+/*
+insert into lecture values
+	('1', 'abc49eb2-0630-4382-98b5-abcfd40627b8','2017-10-16 08:30:00', '2017-10-16 09:30:00'),
+	('2', 'abc49eb2-0630-4382-98b5-abcfd40627b8','2017-10-18 10:30:00', '2017-10-18 11:30:00'),
+	('3', 'abc49eb2-0630-4382-98b5-abcfd40627b8','2017-10-20 14:30:00', '2017-10-20 15:30:00'),
+	('1', 'ddd49eb2-0630-4382-98b5-lolfd40627b8','2017-10-19 15:30:00', '2017-10-19 16:30:00'),
+	('2', 'ddd49eb2-0630-4382-98b5-lolfd40627b8',  ),
+	('1',),
+	('1',);
+*/
