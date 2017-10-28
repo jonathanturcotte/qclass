13fb8f2ad43ff02af3df4d7c0bfb82db1d02ca93
jonathanturcottte <jonathanturcotte@outlook.com>
2017-10-28 18:04:41 -0400

    Dynamically create the navbar

--- public/main.js
+++ public/main.js
@@ -1,17 +1,21 @@
 var NavBar = require('./navbar');
 
-var SignInApp = function () {
-    this.SiteName = "Q-Class";
-};
+SITE_NAME = "Q-Class";
+
+var SignInApp = function () {};
 
 /**
  * Build some common DOM items, then determine if we should
  * build the professor site or student sign in site
  */
 SignInApp.prototype.init = function () {
+    document.title = SITE_NAME;
     this.navbar = new NavBar();
 
-    this.isProfessor() ? this.buildProfDOM() : this.buildStudentDOM(); // jshint ignore:line
+    // Check if we're logged in as a professor or not
+    isProf = this.isProfessor();
+
+    isProf ? this.buildProfDOM() : this.buildStudentDOM(); // jshint ignore:line
 };
 
 /**
@@ -28,7 +32,7 @@ SignInApp.prototype.isProfessor = function (id) {
  * Build the professor DOM
  */
 SignInApp.prototype.buildProfDOM = function () {
-
+    this.navbar.buildProfNavbar();
 };
 
 /**
--- public/navbar.js
+++ public/navbar.js
@@ -1,34 +1,69 @@
-// Items to be added to the NavBar. Items are added in order
-// of their index. Label is the the text that will be displayed
-// profOnly indicates if only professors should see this
+// Items to be added to the NavBar. Items are added in order that they
+// appear in the array. Label is the the text that will be displayed
+// profOnly indicates if only professors should see this. Text indicates
+// whether an item should show up as plain text. If false, it shows as a link.
 NAVBAR_ITEMS = [
-    { label: "Welcome ", index: 0, profOnly: true  },
-    { label: "Sign-out", index: 1, profOnly: false }
+    // { label: "Signed in as ", id: "greeting",  text: true,  profOnly: true  },
+    { label: "Sign-out",      id: "signout",   text: false, profOnly: false }
 ];
 
 var NavBar = function () {
-    this._$element = $('#navbar');
+    this._$element = $('.navbar');
 };
 
 /**
- * Initialize the navbar, and enable or disable professor buttons
+ * Build the professor navbar
  */
-NavBar.prototype.init = function (enableProfItems) {
-
+NavBar.prototype.buildProfNavbar = function () {
+    buildNavbar.call(this, true);
 };
+
+/**
+ * Build the student navbar
+ */
+NavBar.prototype.buildStudentNavbar = function () {
+    buildNavbar.call(this, false);
+};
+
+///////////////////////
+// Private Functions //
+///////////////////////
 
 /**
  * Build the simple navbar
  */
-NavBar.prototype.buildNavbar = function () {
-    
-};
+function buildNavbar (showProf) {
+    // Make sure to clear any old navbar
+    this._$element.empty();
 
-/**
- * Add professor specific items to the navbar
- */
-NavBar.prototype.buildProfNavbar = function () {
+    // Create the basic structure
+    var $siteName = $('<a class="navbar-brand" href="#">').text(document.title),
+        $linkDiv  = $('<div id="navbar" class="collapse navbar-collapse justify-content-end">'),
+        $linkList = $('<ul class="nav navbar-nav navbar-right">');
 
-};
+    // Create a list tag for each navbar item
+    NAVBAR_ITEMS.forEach(function (item) {
+        if (!item.profOnly || showProf){
+            var $li = $('<li class="nav-item">'),
+                $el;
+
+            // TODO - Decide if we even want the greeting
+            // Special case for the greeting
+            // if (item.id === 'greeting') {
+            //     $el = $('<p class="navbar-text">');
+            // } else {
+            $el = $('<a class="nav-link">');
+            // }
+
+            // Add item specific information, then append them
+            $el.text(item.label).attr('id', item.id).appendTo($li);
+            $li.appendTo($linkList);
+        }
+    });
+
+    // Add everything together
+    $linkList.appendTo($linkDiv);
+    this._$element.append($siteName).append($linkDiv);
+}
 
 module.exports = NavBar;
