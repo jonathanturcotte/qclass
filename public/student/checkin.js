ae77fee64cc8879da2f3f7ac580221b723d809f8
Omar Sandarusi <omar.sandarusi@gmail.com>
2017-11-20 20:11:59 -0500

    Added submit button in addition to the enter keypress

--- public/student/checkin.js
+++ public/student/checkin.js
@@ -15,7 +15,12 @@ function build() {
         class: "student-label",
         text: "Enter session code:"
     }).appendTo($labelDiv);
+    
+    // Hidden alert for success or failure messages
+    this.$alert = $('<div>', { class: 'alert-container collapse' })
+        .appendTo(this._$element);
 
+    // Text field
     this.$input = $('<input>', {
         class: "student-input",
         type: "text",
@@ -24,35 +29,36 @@ function build() {
         autofocus: true,
         inputmode: "verbatim",
         maxlength: 5,
-        minlength: 5,
+        minlength: 5
     }).keypress(function(e) {
         if (e.keyCode == 13) { // if Enter
             this.$button.click();
         } 
     }.bind(this)).appendTo($inputDiv);
 
-    this.$button = $('<button>', { class: 'submit-button' })
-        .click(function() {
-            var code = this.$input.val();
-            if (!code) {
-                this.displayAlert('Code cannot be empty', true);
-            } else if (code.length < 5) {
-                this.displayAlert('Code is too short', true);
-            } else if (code.length > 5) {
-                this.displayAlert('Code is too long', true);
-            }
-            else {
-                $.post({ url: '/student/sign-in/' + code })
-                .done(success.bind(this))
-                .fail(failure.bind(this));
-            }
-        }.bind(this));
-
-    this.$alert = $('<div>', { class: 'alert-container collapse' })
-        .appendTo(this._$element);
+    // Submit button
+    this.$button = $('<button>', { 
+        class: 'student-submit-button btn btn-primary',
+        text: 'Sign In'
+    }).click(function() {
+        var code = this.$input.val();
+        if (!code) {
+            this.displayAlert('Code cannot be empty', true);
+        } else if (code.length < 5) {
+            this.displayAlert('Code is too short', true);
+        } else if (code.length > 5) {
+            this.displayAlert('Code is too long', true);
+        }
+        else {
+            $.post({ url: '/student/sign-in/' + code })
+            .done(success.bind(this))
+            .fail(failure.bind(this));
+        }
+    }.bind(this));
 
     $labelDiv.appendTo(this._$element);
     $inputDiv.appendTo(this._$element);
+    this.$button.appendTo(this._$element);
 
     // Functions
 
