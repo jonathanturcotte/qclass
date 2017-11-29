e645836bac7e4e9297d3f2c4513c2614b1db79e4
Omar Sandarusi <omar.sandarusi@gmail.com>
2017-11-28 20:44:26 -0500

    Added api/auth.js that was omitted

---
+++ api/auth.js
@@ -0,0 +1,14 @@
+/**
+ * Authenticates the user - currently just checks the existence of the cookie 
+ * Authenticated users will have their information stored in req.user
+ * @param {Request} req 
+ * @param {Response} res
+ * @param {Function} next
+ */
+exports.authenticate = function (req, res, next) {
+    var netID = req.cookies.netId;
+    if (!netID) 
+        return routeHelper.sendError(res, null, 'Forbidden - No netID provided', 403);
+    req.user = { netID: netID };
+    next();
+}
