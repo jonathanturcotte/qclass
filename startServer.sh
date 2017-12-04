8d9e4da0bb55c4183636def42f210b22a3089015
Jonathan Turcotte <jonathanturcotte@users.noreply.github.com>
2017-12-04 16:04:13 -0500

    startServer script now installs any new dependencies

--- startServer.sh
+++ startServer.sh
@@ -2,6 +2,7 @@
 
 # Make sure that the project is up to date.
 # Update the mysql database
+# Install any new dependencies
 # Start the node server
 # Webpack and uglify the JS
 
@@ -11,5 +12,6 @@
 git checkout master
 git pull origin master
 mysql -u root -ppassword < SISystem.sql
+npm install
 npm run release
 npm start
