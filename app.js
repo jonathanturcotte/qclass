55c8c761b986dbb01099697cba3f7c60c31076bf
root <root@ip-172-31-20-50.ca-central-1.compute.internal>
2018-01-10 21:25:21 +0000

    Enforce authentication on all routes and requests, bounce to test IdP

--- app.js
+++ app.js
@@ -39,36 +39,36 @@ var express       = require('express'),
     https        = https.createServer(sslOptions, app).listen(443);
 
 // Use Helmet to help cover some common header security issues
-app.use(helmet({
-    referrerPolicy: { policy: 'no-referrer' },
-    contentSecurityPolicy: {
-        directives: {
-            defaultSrc: ["'self'",
-                'data:',
-                'https://idptest.queensu.ca',           // IdP for SSO
-                'https://code.jquery.com',              // jQuery
-                'https://cdnjs.cloudflare.com',         // Popper (Bootstrap), Toastr, Underscore
-                'https://maxcdn.bootstrapcdn.com',      // Bootstrap
-                'https://use.fontawesome.com'],         // Font Awesome
-            styleSrc: ["'self'",
-                "'unsafe-inline'",
-                'https://maxcdn.bootstrapcdn.com',      // Bootstrap
-                'https://cdnjs.cloudflare.com'],        // Toastr
-            imgSrc: ["'self'", 'data:']
-        }
-    }
-}));
+//app.use(helmet({
+    //referrerPolicy: { policy: 'no-referrer' },
+    //contentSecurityPolicy: {
+        //directives: {
+            //defaultSrc: ["'self'",
+                //'data:',
+                //'https://idptest.queensu.ca',           // IdP for SSO
+                //'https://code.jquery.com',              // jQuery
+                //'https://cdnjs.cloudflare.com',         // Popper (Bootstrap), Toastr, Underscore
+                //'https://maxcdn.bootstrapcdn.com',      // Bootstrap
+                //'https://use.fontawesome.com'],         // Font Awesome
+            //styleSrc: ["'self'",
+                //"'unsafe-inline'",
+                //'https://maxcdn.bootstrapcdn.com',      // Bootstrap
+                //'https://cdnjs.cloudflare.com'],        // Toastr
+            //imgSrc: ["'self'", 'data:']
+        //}
+    //}
+//}));
 
 // Initialize the socketIO
-app.io = io.initialize();
+//app.io = io.initialize();
 
 // Ensure that all traffic is being routed through https
-app.all('*', function (req, res, next) {
-    if (req.secure)
-        return next();
-    else
-        res.redirect('https://' + req.hostname + req.url);
-});
+//app.all('*', function (req, res, next) {
+    //if (req.secure)
+        //return next();
+    //else
+        //res.redirect('https://' + req.hostname + req.url);
+//});
 
 // TODO: Add favicon
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -76,42 +76,76 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
     secret: 'Whocansaywherethewindblows',
     resave: true,
     saveUninitialized: true
  }));
-app.use(passport.initialize());
-app.use(passport.session());
+
+passport.serializeUser(function(user, done){
+    console.log("SAML - Serialize User");
+    done(none, user);
+});
+
+passport.deserializeUser(function(user, done){
+    console.log("SAML - Deserialize User");
+    done(none, user);
+});
 
 // TODO: Somehow skip this for localhost testing
 // see: https://stackoverflow.com/questions/24419814/passport-saml-and-saml-encryption
 var passportStrat = new SamlStrategy({
-        callbackUrl      : 'https://qclass.ca/login/callback',                       // The login callback
-        logoutCallbackUrl: 'https://qclass.ca/logout/callback',                      // Logout callback 
-        entryPoint       : 'https://idptest.queensu.ca/idp/shibboleth',              // location of IDP
-        logoutUrl        : 'https://idptest.queensu.ca/idp/Shibboleth.sso/SLO/POST', // URL for logging out on the IDP
-        issuer           : 'https://qclass.ca',                                      // The identifier for our SP
-        cert             : fs.readFileSync('sso/idp.crt', 'utf8'),                   // X509 cert for the idp, needs to be all on one line
-        decryptionPvk    : fs.readFileSync(keyPath, 'utf8')                          // Our private key
+        callbackUrl      : 'https://qclass.ca/login/callback',                          // The login callback
+        logoutCallbackUrl: 'https://qclass.ca/logout/callback',                         // Logout callback
+        entryPoint       : 'https://idptest.queensu.ca/idp/profile/SAML2/Redirect/SSO', // location of IDP
+        logoutUrl        : 'https://idptest.queensu.ca/idp/Shibboleth.sso/SLO/POST',    // URL for logging out on the IDP
+        issuer           : 'https://qclass.ca',                                         // The identifier for our SP
+        cert             : fs.readFileSync('sso/idp.crt', 'utf8'),                      // X509 cert for the idp, needs to be all on one line
+        decryptionPvk    : fs.readFileSync(keyPath, 'utf8')                             // Our private key
     }, function (profile, done) {
-        return done(null, profile); // TODO: Replace with something more meaningful    
+        console.log("SAML - In passport callback");
+        return done(null, profile); // TODO: Replace with something more meaningful
     }
 );
 
-console.log("\nGenerating test SAML XML\n");
 var cert = fs.readFileSync(certPath, 'utf8');
-//console.log(cert);
-var res = passportStrat.generateServiceProviderMetadata(cert);
-fs.writeFileSync('sso/sp-metadata.xml', res);
-
 passport.use(passportStrat);
+app.use(passport.initialize());
+app.use(passport.session());
 
-//app.use(auth.authenticate); // Run authentication first when any route is called
+// Enforce authentication for all requests
+// Must remain above the other routes/middlewares to force
+// authentication for all the subsequent ones
+app.get('*', passport.authenticate('saml', { successRedirect: '/', failureRedirect: '/login/fail' }));
+
+// Serve the static pages
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Our routes
 app.use('/', general);
 app.use('/student', student);
 app.use('/professor', professor);
+
+
+app.post('/login/callback', function (req, res, next) {
+    console.log("SAML - CALLBACK");
+    passport.authenticate('saml2', {'successRedirect': '/', 'failureRedirect': '/login/fail' });
+});
+
+app.get('/login/fail',
+    function(req, res) {
+        console.log("SAML - get /login/fail");
+        res.send(401, 'Login failed');
+    }
+);
+
+app.get('/Shibboleth.sso/Metadata',
+    function(req, res) {
+        console.log('SAML - get metadata');
+        res.type('application/xml');
+        res.send(200, samlStrategy.generateServiceProviderMetadata(cert));
+    }
+);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
